b867ae71f16978fc038f26891936da702350e469
kelly1422 <kelly1422@naver.com>
2022-08-06 20:56:37 +0900

    use AutoLayout

---
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by kelly on 2022/07/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var colorView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func tapChangeColorButton(_ sender: UIButton) {
+        self.colorView.backgroundColor = UIColor.blue
+        print("a")
+    }
+    
+}
+
