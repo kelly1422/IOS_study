6d752ac9668acca79e22879620f88b6650b1d1a8
kelly1422 <kelly1422@naver.com>
2022-10-15 03:17:30 +0900

    [feat]: set todolist tableView

---
+++ ToDoList/ToDoList/Task.swift
@@ -0,0 +1,8 @@
+//
+//  Task.swift
+//  ToDoList
+//
+//  Created by kelly on 2022/10/15.
+//
+
+import Foundation
--- ToDoList/ToDoList/ViewController.swift
+++ ToDoList/ToDoList/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapEditButton(_ sender: UIBarButtonItem) {
+        var alert = UIAlertController(title: "할 일 등록", message: nil, preferredStyle: .alert)
+        var addButton = UIAlertAction(title: "등록", style: .default) { _ in
+            
+        }
+        var cancelButton = UIAlertAction(title: "취소", style: .cancel) { _ in
+            
+        }
+        alert.addAction(cancelButton)
+        alert.addAction(addButton)
+        alert.addTextField { UITextField in
+            UITextField.placeholder = "할 일을 입력해주세요."
+        }
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func tapAddButoon(_ sender: UIBarButtonItem) {
+    
+    }
 }
 
