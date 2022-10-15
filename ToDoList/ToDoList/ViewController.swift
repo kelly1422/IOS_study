b515d18d88ba6e44e48cbc36c5a3269f32c9bbd3
kelly1422 <kelly1422@naver.com>
2022-10-15 22:42:42 +0900

    [feat]: save data in UserDefaults & add done checkbox

--- ToDoList/ToDoList/ViewController.swift
+++ ToDoList/ToDoList/ViewController.swift
@@ -10,18 +10,31 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
-    var tasks = [Task]()
+    var tasks = [Task]() {
+        didSet {
+            self.saveTasks()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        self.loadTasks()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func tapEditButton(_ sender: UIBarButtonItem) {
+    
+    }
+    
+    @IBAction func tapAddButoon(_ sender: UIBarButtonItem) {
         let alert = UIAlertController(title: "할 일 등록", message: nil, preferredStyle: .alert)
         let addButton = UIAlertAction(title: "등록", style: .default) { [weak self] _ in
             guard let title = alert.textFields?[0].text else { return }
             let newTask = Task(title: title, done: false)
             self?.tasks.append(newTask)
+            self?.tableView.reloadData()
         }
         let cancelButton = UIAlertAction(title: "취소", style: .cancel, handler: nil)
         alert.addAction(cancelButton)
@@ -32,8 +45,51 @@ class ViewController: UIViewController {
         self.present(alert, animated: true, completion: nil)
     }
     
-    @IBAction func tapAddButoon(_ sender: UIBarButtonItem) {
+    private func loadTasks() {
+        let userDefaults = UserDefaults.standard
+        guard let data = userDefaults.object(forKey: "tasks") as? [[String: Any]] else { return }
+        self.tasks = data.compactMap({
+            guard let title = $0["title"] as? String else { return nil }
+            guard let done = $0["done"] as? Bool else { return nil }
+            return Task(title: title, done: done)
+        })
+    }
     
+    private func saveTasks() {
+        let userDefaults = UserDefaults.standard
+        let data = tasks.map {
+            [
+                "title": $0.title,
+                "done": $0.done
+            ]
+        }
+        userDefaults.set(data, forKey: "tasks")
     }
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let task = self.tasks[indexPath.row]
+        cell.textLabel?.text = task.title
+        if task.done {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        var task = self.tasks[indexPath.row]
+        task.done = !task.done
+        tasks[indexPath.row] = task
+        self.tableView.reloadRows(at: [indexPath], with: .automatic)
+    }
+}
